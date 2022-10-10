30dd79bc81e5ae894835ddf22212ff7917d07073
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2022-10-10 12:33:57 -0500

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -614,10 +614,9 @@ var utcTime = date.toUTCString();
          querySnapshot.forEach((doc) => {
 		var nodata = "";
             // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-	    var dates = new Date(doc.data().date).toLocaleString();
-  	    document.write('<tr><td>' + doc.data().firstname + '</td><td>' + doc.data().lastname + '</td><td>' + doc.data().company + '</td><td>' + dates + '</td><td>' + doc.data().email + '</td><td><a href="https://aquavisitorsystem.github.io/?iPadid=' + doc.data().key + '">Select</a></td></tr>');
-	});
+         var dates = new Date(doc.data().date).toLocaleString();
+          document.write('<tr><td>' + doc.data().login + '</td><td>' + doc.data().firstname + '</td><td>' + doc.data().lastname + '</td><td>' + doc.data().company + '</td><td>' + dates + '</td><td>' + doc.data().email + '</td><td>' + doc.data().message + '</td><td>' + doc.data().checkin + '</td><td>' + doc.data().checkout + '</td><td><a href="https://aquavisitorsystem.github.io/?id=' + doc.data().key + '">Click here</a></td></tr>');
+		});
 		   document.write("</table>");
 		// let sendingText = "https://aquameeting.github.io/?ipad=Yes"
 	           document.head.innerHTML = header;
