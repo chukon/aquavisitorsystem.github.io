a8e08a50a9bcfc61b6f98642442875eebf92d5cc
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2022-10-19 10:14:22 -0500

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -289,10 +289,10 @@ var utcTime = date.toUTCString();
 	   document.cookie = "checkin=" + get_id + "; expires=" + utcTime + ";";
 	  //document.cookie = "YOUR_COOKIE=yes; expires=" + utcTime + ";";
 	   document.write("<center>");
-	  document.write('<img id="logo" src="aqua.jpg" width="700px">');
+	  document.write('<img id="logo" src="aqua.jpg" width="800px">');
 	    document.write("<p style='font-size:47px;line-height: 0.9;margin: 15;'>Guest: <b>" + varFName + " " + varLName + "</b></p>");
 	  document.write('<canvas id="qrcodes"></canvas>');
-	  document.write("<p style='font-size:25px;color: black;'>Company: " + varcp + "</p>");
+	  document.write("<p style='font-size:25px;color: black;margin: 15;'>Company: " + varcp + "</p>");
 	  // document.write("<p style='font-size:16px;color: black;'><br><br><br>printed: " + NowTime + "</p></center>");
    var qrcode = new QRious({
   element: document.getElementById("qrcodes"),
