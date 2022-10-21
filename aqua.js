ecbbc0d832595d4cbf0e57667e1bf2b5d6c1d6cd
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2022-10-21 07:02:02 -0500

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -910,6 +910,7 @@ if (login.value != null &&  login.value != '' && fname.value != null &&  fname.v
        document.getElementById('get_id').style.display = 'none';
       document.getElementById('get_msg').style.display = 'block';
          document.getElementById('get_id2').style.display = 'none';
+	 document.getElementById('qrcode').style.display = 'none';      
 	       
        }
        
