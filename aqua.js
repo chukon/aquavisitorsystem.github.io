4e756ff396f99072ca44e34de09667e8ad541ead
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2022-10-21 06:33:12 -0500

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -937,8 +937,8 @@ if (login.value != null &&  login.value != '' && fname.value != null &&  fname.v
        document.getElementById('get_id').style.display = 'block';
       document.getElementById('get_msg').style.display = 'block';
           document.getElementById('get_id2').style.display = 'block';
-	 document.getElementById('loginlabel').innerText = 'Aqua Employee Network ID OR Keyword';
- document.getElementById('get_id').innerText = 'Examples: [Network ID] ckonkol | keywords: today,date,all,inactive';
+ document.getElementById('loginlabel').innerText = 'Aqua Employee Network ID OR Keyword';
+ document.getElementById('login')[0].placeholder = 'Examples [Network ID]:ckonkol,keywords: today,date,all,inactive';
        }
        
        // <button id="checkin" type="button">Everything look ok? Tap Here to Check-In</button>
