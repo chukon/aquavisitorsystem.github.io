cefba27341f566031951b34249342bb102253e15
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2022-11-18 09:00:37 -0600

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -149,10 +149,7 @@ var fldkey;
 	 
 	 var updateresetvisit = function(data){
 		 clear()
-		 document.body.innerHTML = ''; 
-		document.write("");
-		 window.location = "https://aquavisitorsystem.github.io";
-		 let text = "Are you sure you want to reset check-in/check-out data?\n\nThis cannot be undone!\n\nClick 'OK' to reset data\nClick 'Cancel' to go back!";
+			 let text = "Are you sure you want to reset check-in/check-out data?\n\nThis cannot be undone!\n\nClick 'OK' to reset data\nClick 'Cancel' to go back!";
 	if (confirm(text) == true) {	
 		document.write("");
         var db = firebase.firestore();
