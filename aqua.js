afb0668336cd199aff08d8f01bb0acd619626d5b
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2022-11-26 13:12:30 -0600

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -702,6 +702,7 @@ var utcTime = date.toUTCString();
                alert("Enter your Network Login ID above & try again!");
           }else{
        get_login  = get_login.toString();
+       get_login = get_login.trim().toUpperCase();
      var title = "<center><h1>Aqua-Aerobic Systems Check-in/out Visitor Schedule</h1><h2>Active Visitor Schedule(s) for: " + get_login + "</h2><a href='https://aquavisitorsystem.github.io/'>Go Home</a><br><br></center>";
      console.log(get_login);
       var header = "<head><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'><style>table, td, th {  border: 1px solid #cbbbbb;  text-align: left;}table {  border-collapse: collapse;  width: 100%;}th, td {  padding: 15px;} tr:nth-child(even) {  background-color: #dddddd;}</style></head>";
