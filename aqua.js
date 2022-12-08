a544b2ab1a1b876b221b73348285ca51d6062242
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2022-12-08 07:14:57 -0600

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -340,7 +340,7 @@ var fldkey;
       document.getElementById('get_msg').style.display = 'none';
 		 document.getElementById('update_db').innerText = 'Update';
 		  document.getElementById("emaillink").innerHTML = "";
-		 document.getElementById("remove").innerHTML = "<b style='color: red;'>1) IF NEEDED, UPDATE ABOVE FIELDS 2) TAP GREEN BUTTON BELOW TO CHECK-IN/OUT</b>";
+		 document.getElementById("remove").innerHTML = "<i style='color: lightgray;'>If needed, update your information before tapping button below</i>";
 		  document.getElementById('remove').style.fontWeight = 'normal';
 		 document.getElementById('update_db').style.width = 'min-content';
 		  document.getElementById('checkin').style.display = 'block';
