253c5f12136bd3e59c5ff87c6be410b59a76d7c9
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2022-12-22 10:54:42 -0600

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -712,7 +712,7 @@ var utcTime = date.toUTCString();
 		
 		 var get_login=prompt("Enter Guest Last Name To Search","Enter Guest Last Name");
          if (get_login  === null || get_login === "Enter Last Name") {
-               alert("Enter Guest Last Name above & try again!");
+               alert("Try Again! Please Enter Guest Last Name.");
           }else{
        get_login  = get_login.toString();
        get_login = get_login.trim().toUpperCase();
