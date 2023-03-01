159d9f48924cd7356dd858024f58ad4ad97e0239
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2023-03-01 13:13:08 -0600

    ck

--- aqua.js
+++ aqua.js
@@ -931,7 +931,7 @@ document.getElementById('back').style.display = 'block';
             //var title = "<center><h1>Aqua-Aerobic Systems Visitor Schedule (active report)</h1><h2>" +  "<label id='numcount'></label>" + + " Active Visitor Schedule(s) for: " + get_login + "</h2><a href='https://aquavisitorsystem.github.io/'>Go Home</a><br><br></center>";
             //var msg = '<button  onclick="loaddb(' + username + ')"><u>Example button</u></button>';
       
-            var msg = "<input type='button' id='btnPrint' onclick='loaddb();' value='Show All Active Schedules for: " + fldloginID  + "'/>";
+            var msg = "<input type='button' id='btnPrint' onclick='loaddb();' value='Show All Schedules for: " + fldloginID  + "'/>";
       
             var title = "<center><h1>Aqua-Aerobic Systems Visitor Schedule (Aqua UserID Report)</h1><h2>" + "<label id='numcount'></label>" + " Active Visitor Schedule(s) for: " + get_login + "<br><small>(showing today and beyond only)</small></h2><a href='https://aquavisitorsystem.github.io/'>Go Home</a>" + "<br><br>" + msg  +   "<br><br></center>";
   
