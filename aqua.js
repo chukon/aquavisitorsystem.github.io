1450fa07f760b3f453958fe3d268fe946cca7b2c
Chuck Konkol - AquaDevelopers <ignitetheozarks@gmail.com>
2023-12-20 10:35:06 -0600

    cc

--- aqua.js
+++ aqua.js
@@ -1455,9 +1455,9 @@ document.getElementById('back').style.display = 'block';
       
             var msg = "<input type='button' id='btnPrint' onclick='loaddb();' value='Show All Schedules for: " + fldloginID  + "'/>";
 
-
+            var goback = ' | <a href="javascript:history.back();">Go Back</a>';
       
-            var title = "<center><h1>Aqua-Aerobic Systems Visitor Schedule (My VMS Report)</h1><h2>" + "<label id='numcount'></label>" + " Active Visitor Schedule(s) for: <a href='https://aquavisitorsystem.github.io/?userid=" + get_login + "&report=active'>" + get_login + "</a><br><small style='font-size: 16px;color: blue;'>report created on: " +  gtodaysdate + "</small></h2><div id='homes'><a href='https://aquavisitorsystem.github.io/'>Go Home</a>" + "<br></div><br>" + msg  +   "<br><br></center>";
+            var title = "<center><h1>Aqua-Aerobic Systems Visitor Schedule (My VMS Report)</h1><h2>" + "<label id='numcount'></label>" + " Active Visitor Schedule(s) for: <a href='https://aquavisitorsystem.github.io/?userid=" + get_login + "&report=active'>" + get_login + "</a><br><small style='font-size: 16px;color: blue;'>report created on: " +  gtodaysdate + "</small></h2><div id='homes'><a href='https://aquavisitorsystem.github.io/'>Go Home</a>" + goback + "<br></div><br>" + msg  +   "<br><br></center>";
   
             document.write(title);
             document.write(printnow);
